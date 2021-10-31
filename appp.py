a4d270cb78019a4c8a0087ae3b05103d1a265e3e
menglingchen <60646146+menglingchen@users.noreply.github.com>
2021-10-31 23:45:16 +0800

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.title('Hello,Angela!')
+
+if __name__ == "__main__":
+    main()
